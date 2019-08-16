b8e625c8c3cdd8e9a7c0a836cbbb83060f1832e7
Andrew Bares <anbare@microsoft.com>
2019-08-16 15:14:34 -0700

    Add CachedComputation to BindableWithDispatcher

--- ToolsPortable/ToolsPortable/BindableBaseWithPortableDispatcher.cs
+++ ToolsPortable/ToolsPortable/BindableBaseWithPortableDispatcher.cs
@@ -199,5 +199,120 @@ namespace ToolsPortable
         }
 
         public static Func<PortableDispatcher> ObtainDispatcherFunction { get; set; }
+
+        private Dictionary<string, CachedComputationProperty> m_cachedComputationProperties;
+        protected T CachedComputation<T>(Func<T> computation, string[] dependentOn, [CallerMemberName]string propertyName = null)
+        {
+            if (m_cachedComputationProperties == null)
+            {
+                m_cachedComputationProperties = new Dictionary<string, CachedComputationProperty>();
+            }
+
+            CachedComputationProperty prop;
+            if (!m_cachedComputationProperties.TryGetValue(propertyName, out prop))
+            {
+                prop = new CachedComputationProperty(this, propertyName, delegate { return computation(); });
+                m_cachedComputationProperties[propertyName] = prop;
+
+                foreach (var dependentOnProperty in dependentOn)
+                {
+                    ListenToProperty(dependentOnProperty, prop.NotifyDependentOnValueChanged);
+                }
+            }
+
+            object val = prop.GetValue();
+            if (val == null)
+            {
+                return default(T);
+            }
+            return (T)val;
+        }
+
+        private class CachedComputationProperty
+        {
+            private bool m_isUpToDate;
+            private object m_currentValue;
+            private BindableBaseWithPortableDispatcher m_parent;
+            private Func<object> m_computation;
+            private string m_propertyName;
+            private bool m_hasBeenRequested;
+
+            public CachedComputationProperty(BindableBaseWithPortableDispatcher parent, string thisPropertyName, Func<object> computation)
+            {
+                m_parent = parent;
+                m_computation = computation;
+                m_propertyName = thisPropertyName;
+            }
+
+            public object GetValue()
+            {
+                m_hasBeenRequested = true;
+
+                if (!m_isUpToDate)
+                {
+                    m_currentValue = m_computation();
+                    m_isUpToDate = true;
+                }
+
+                return m_currentValue;
+            }
+
+            public void NotifyDependentOnValueChanged()
+            {
+                // Only if it's up-to-date and has been requested should we compute
+                if (m_isUpToDate && m_hasBeenRequested)
+                {
+                    m_isUpToDate = false;
+                    var currVal = m_currentValue;
+                    var newVal = GetValue();
+                    if (!object.Equals(currVal, newVal))
+                    {
+                        // Change this back to false, so that we can see if anyone actually asks for the new value again,
+                        // since maybe they've stopped listening since then, and we can stop sending notifications
+                        m_hasBeenRequested = false;
+
+                        // Notify it was changed
+                        m_parent.OnPropertyChanged(m_propertyName);
+                    }
+                }
+            }
+        }
+
+        private Dictionary<string, List<Action>> m_propertyChangedActions;
+        /// <summary>
+        /// Perform an action if the property was changed
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="action"></param>
+        protected void ListenToProperty(string propertyName, Action action)
+        {
+            if (m_propertyChangedActions == null)
+            {
+                m_propertyChangedActions = new Dictionary<string, List<Action>>();
+
+                this.PropertyChanged += OwnPropertyChanged;
+            }
+
+            List<Action> actions;
+            if (!m_propertyChangedActions.TryGetValue(propertyName, out actions))
+            {
+                actions = new List<Action>();
+                m_propertyChangedActions[propertyName] = actions;
+            }
+
+            actions.Add(action);
+        }
+
+        private void OwnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            List<Action> actions;
+            if (m_propertyChangedActions.TryGetValue(e.PropertyName, out actions))
+            {
+                foreach (var a in actions)
+                {
+                    a();
+                }
+            }
+        }
     }
 }
