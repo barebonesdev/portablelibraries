81c4ffdf041888a40758c85c70cf75c64850e70e
Andrew Leader <followthatleader@outlook.com>
2020-09-12 11:13:32 -0700

    Initial version

---
+++ ToolsPortable/ToolsPortable/InputValidationState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolsPortable
+{
+    public class InputValidationState
+    {
+        public static readonly InputValidationState Valid = new InputValidationState();
+
+        public static InputValidationState Invalid(string errorMessage)
+        {
+            return new InputValidationState()
+            {
+                ErrorMessage = errorMessage
+            };
+        }
+
+        private InputValidationState() { }
+
+        public string ErrorMessage { get; private set; }
+    }
+}
--- ToolsPortable/ToolsPortable/StringTools.cs
+++ ToolsPortable/ToolsPortable/StringTools.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ToolsPortable
 {
@@ -67,6 +68,11 @@ namespace ToolsPortable
                 return true;
 
             return false;
+        }
+
+        public static bool IsEmailValid(string email)
+        {
+            return Regex.IsMatch(email, @"^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f] |\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$");
         }
 
         public static string ToString(IEnumerable items, string separator)
